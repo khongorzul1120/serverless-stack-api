680f408f6dae81aa34ef3fd2da7f98931d9f5697
Khongorzul Odonkhuu <khongorzulodonkhuu@gmail.com>
2020-01-19 17:17:54 +0800

    Adding our Serverless API

---
+++ billing.js
@@ -0,0 +1,24 @@
+import stripePackage from "stripe";
+import { calculateCost } from "./libs/billing-lib";
+
+
+export async function main(event, context) {
+  const { storage, source } = JSON.parse(event.body);
+  const amount = calculateCost(storage);
+  const description = "Scratch charge";
+
+  // Load our secret key from the  environment variables
+  const stripe = stripePackage(process.env.stripeSecretKey);
+
+  try {
+    await stripe.charges.create({
+      source,
+      amount,
+      description,
+      currency: "usd"
+    });
+    return success({ status: true });
+  } catch (e) {
+    return failure({ message: e.message });
+  }
+}
